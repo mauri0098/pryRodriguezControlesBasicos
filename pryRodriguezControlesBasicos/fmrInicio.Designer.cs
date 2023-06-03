200138f9fc13028f0d5bac0786c318efa292c569
mauri0098 <maurirodriguez0098@gmail.com>
2023-06-03 14:26:55 -0300

    26/05/2023

--- pryRodriguezControlesBasicos/fmrInicio.Designer.cs
+++ pryRodriguezControlesBasicos/fmrInicio.Designer.cs
@@ -46,26 +46,26 @@
             // Ibltexto
             // 
             this.Ibltexto.AutoSize = true;
-            this.Ibltexto.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Ibltexto.Location = new System.Drawing.Point(146, 72);
+            this.Ibltexto.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ibltexto.Location = new System.Drawing.Point(146, 68);
             this.Ibltexto.Name = "Ibltexto";
-            this.Ibltexto.Size = new System.Drawing.Size(35, 15);
+            this.Ibltexto.Size = new System.Drawing.Size(42, 18);
             this.Ibltexto.TabIndex = 0;
             this.Ibltexto.Text = "Texto";
             this.Ibltexto.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtTexto
             // 
-            this.txtTexto.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTexto.Location = new System.Drawing.Point(143, 35);
+            this.txtTexto.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTexto.Location = new System.Drawing.Point(143, 29);
             this.txtTexto.Name = "txtTexto";
-            this.txtTexto.Size = new System.Drawing.Size(181, 23);
+            this.txtTexto.Size = new System.Drawing.Size(181, 26);
             this.txtTexto.TabIndex = 1;
             this.txtTexto.TextChanged += new System.EventHandler(this.txtUsuario_TextChanged);
             // 
             // cmdAsignar
             // 
-            this.cmdAsignar.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAsignar.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdAsignar.Location = new System.Drawing.Point(181, 138);
             this.cmdAsignar.Name = "cmdAsignar";
             this.cmdAsignar.Size = new System.Drawing.Size(105, 25);
@@ -77,10 +77,10 @@
             // optAzul
             // 
             this.optAzul.AutoSize = true;
-            this.optAzul.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optAzul.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.optAzul.Location = new System.Drawing.Point(168, 103);
             this.optAzul.Name = "optAzul";
-            this.optAzul.Size = new System.Drawing.Size(48, 19);
+            this.optAzul.Size = new System.Drawing.Size(53, 22);
             this.optAzul.TabIndex = 3;
             this.optAzul.TabStop = true;
             this.optAzul.Text = "Azul";
@@ -89,10 +89,10 @@
             // optRojo
             // 
             this.optRojo.AutoSize = true;
-            this.optRojo.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.optRojo.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.optRojo.Location = new System.Drawing.Point(236, 103);
             this.optRojo.Name = "optRojo";
-            this.optRojo.Size = new System.Drawing.Size(50, 19);
+            this.optRojo.Size = new System.Drawing.Size(54, 22);
             this.optRojo.TabIndex = 4;
             this.optRojo.TabStop = true;
             this.optRojo.Text = "Rojo";
@@ -104,7 +104,7 @@
             this.gbAderezo.Controls.Add(this.chkMostaza);
             this.gbAderezo.Controls.Add(this.chkMayonesa);
             this.gbAderezo.Controls.Add(this.chkPicante);
-            this.gbAderezo.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbAderezo.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbAderezo.Location = new System.Drawing.Point(143, 169);
             this.gbAderezo.Name = "gbAderezo";
             this.gbAderezo.Size = new System.Drawing.Size(181, 100);
@@ -117,7 +117,7 @@
             this.chkMostaza.AutoSize = true;
             this.chkMostaza.Location = new System.Drawing.Point(6, 65);
             this.chkMostaza.Name = "chkMostaza";
-            this.chkMostaza.Size = new System.Drawing.Size(73, 19);
+            this.chkMostaza.Size = new System.Drawing.Size(78, 22);
             this.chkMostaza.TabIndex = 8;
             this.chkMostaza.Text = "Mostaza";
             this.chkMostaza.UseVisualStyleBackColor = true;
@@ -127,7 +127,7 @@
             this.chkMayonesa.AutoSize = true;
             this.chkMayonesa.Location = new System.Drawing.Point(6, 42);
             this.chkMayonesa.Name = "chkMayonesa";
-            this.chkMayonesa.Size = new System.Drawing.Size(83, 19);
+            this.chkMayonesa.Size = new System.Drawing.Size(90, 22);
             this.chkMayonesa.TabIndex = 7;
             this.chkMayonesa.Text = "Mayonesa";
             this.chkMayonesa.UseVisualStyleBackColor = true;
@@ -137,7 +137,7 @@
             this.chkPicante.AutoSize = true;
             this.chkPicante.Location = new System.Drawing.Point(6, 19);
             this.chkPicante.Name = "chkPicante";
-            this.chkPicante.Size = new System.Drawing.Size(67, 19);
+            this.chkPicante.Size = new System.Drawing.Size(73, 22);
             this.chkPicante.TabIndex = 6;
             this.chkPicante.Text = "Picante";
             this.chkPicante.UseVisualStyleBackColor = true;
@@ -146,17 +146,17 @@
             // lblEscribir
             // 
             this.lblEscribir.AutoSize = true;
-            this.lblEscribir.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscribir.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEscribir.Location = new System.Drawing.Point(262, 293);
             this.lblEscribir.Name = "lblEscribir";
-            this.lblEscribir.Size = new System.Drawing.Size(50, 15);
+            this.lblEscribir.Size = new System.Drawing.Size(53, 18);
             this.lblEscribir.TabIndex = 9;
             this.lblEscribir.Text = "Escribir";
             this.lblEscribir.Click += new System.EventHandler(this.lblEscribir_Click);
             // 
             // cmdSeleccionar
             // 
-            this.cmdSeleccionar.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSeleccionar.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdSeleccionar.Location = new System.Drawing.Point(143, 289);
             this.cmdSeleccionar.Name = "cmdSeleccionar";
             this.cmdSeleccionar.Size = new System.Drawing.Size(97, 23);
@@ -167,11 +167,11 @@
             // 
             // lstAderezos
             // 
-            this.lstAderezos.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstAderezos.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstAderezos.FormattingEnabled = true;
             this.lstAderezos.Location = new System.Drawing.Point(143, 330);
             this.lstAderezos.Name = "lstAderezos";
-            this.lstAderezos.Size = new System.Drawing.Size(181, 23);
+            this.lstAderezos.Size = new System.Drawing.Size(181, 26);
             this.lstAderezos.TabIndex = 12;
             this.lstAderezos.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
@@ -179,7 +179,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 465);
+            this.ClientSize = new System.Drawing.Size(439, 427);
             this.Controls.Add(this.lstAderezos);
             this.Controls.Add(this.cmdSeleccionar);
             this.Controls.Add(this.lblEscribir);
